4ad26d5cb638bd18958a70f45e857157d18f1ebb
hardik panchal <hardikpanchal2130@gmail.com>
2025-04-09 19:55:59 +0530

    add config

--- server.ts
+++ server.ts
@@ -1,12 +1,14 @@
 import app from './src/app'
+import { config } from './src/config/config';
 
-const startServer = ()=>{
-    const port = process.env.PORT || 3000;
-
-    app.listen(port,()=>{
-        console.log(`Listening on Port ${port}`);
-        
-    })
-}
+const startServer = async () => {
+    
+  
+    const port = config.port || 3000;
+  
+    app.listen(port, () => {
+      console.log(`Listening on port: ${port}`);
+    });
+  };
 
 startServer();
--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import express from 'express'
 const app = express()
 
 //Routes
-app.get('/',(req,res,next)=>{
+app.get('/',(req,res)=>{
     res.json({message:"welcome to the Elib api"})
 })
 
